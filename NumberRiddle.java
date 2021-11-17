e2a4db3ede5d68d49c9f6bf78296910bbc38976f
Zhou <justinz47287@stu.powayusd.com>
2021-11-17 13:08:07 -0800

    test commit

---
+++ NumberRiddle.java
@@ -0,0 +1,29 @@
+public class NumberRiddle{
+
+    public static void main(String[] arg){
+
+        final double startingNum = -1.5;
+
+        double operatingNum = startingNum;
+
+        System.out.println("The starting number is: " + startingNum);
+
+        operatingNum *= 2;
+
+        System.out.println("Double: " + startingNum + " x 2 = " + operatingNum);
+
+        operatingNum += 6;
+
+        System.out.println("Add 6: " + startingNum*2 + " + 6 = " + operatingNum);
+
+        operatingNum /= 2;
+
+        System.out.println("Halve: " + (startingNum * 2 + 6) + " / 2 = " + operatingNum);
+
+        operatingNum -= startingNum;
+
+        System.out.println("Subtract by starting number: " + (startingNum * 2 + 6) / 2 + " - " + startingNum + " = " + operatingNum);
+
+        System.out.println("The final number is 3, as expected.");
+    }
+}
